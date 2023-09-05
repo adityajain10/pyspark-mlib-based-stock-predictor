958d26f03f3f480038595bc4063d63613743bafc
Aditya Jain <aj2529@nyu.edu>
2023-09-04 22:48:17 -0700

    update logic

--- finalStockPrediction.py
+++ finalStockPrediction.py
@@ -1,56 +1,88 @@
-from __future__ import print_function
-
 from pyspark.ml import Pipeline
+from pyspark.ml.regression import GBTRegressor
+from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.evaluation import RegressionEvaluator
-# $example on$
-from pyspark.ml.feature import OneHotEncoderEstimator, VectorAssembler, StringIndexer
-from pyspark.ml.regression import DecisionTreeRegressor
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from pyspark.sql.window import Window
+import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
-    spark = SparkSession \
-        .builder \
-        .appName("SVMTest") \
-        .getOrCreate()
+# Create a Spark session
+spark = SparkSession.builder.appName("ImprovedStockPrediction").getOrCreate()
 
-    # $example on$
-    # Load the data stored in LIBSVM format as a DataFrame.
-    df = spark.read.format("csv").option("header", "true").load("stocks_data.csv")
+# Load the data from the CSV into a DataFrame
+df = spark.read.csv("stocks_data.csv", header=True, inferSchema=True)
 
+# Window function to order by date (for features that require looking at previous rows)
 w = Window.partitionBy().orderBy("date")
 
-df = df.withColumn('diffOpenClose', df.open - df.close)
-df = df.withColumn('diffHighLow', df.high - df.low)
-df = df.withColumn('target', F.when(F.lag(df.close).over(w) < df.close, 'yes').otherwise('no'))
-df.drop('date')
-categoricalColumns = ['high', 'low', 'open', 'close']
-stages = []
+# Feature Engineering
+# 1. Create a lagged column for the 'close' price (i.e., previous day's close)
+df = df.withColumn("lag_close_1", F.lag(df.close).over(w))
 
-for categoricalCol in categoricalColumns:
-    stringIndexer = StringIndexer(inputCol=categoricalCol, outputCol=categoricalCol + 'Index')
-    encoder = OneHotEncoderEstimator(inputCols=[stringIndexer.getOutputCol()], outputCols=[categoricalCol + "classVec"])
-    stages += [stringIndexer, encoder]
+# 2. Calculate daily return - how much did the price change in percentage from opening to closing
+df = df.withColumn("daily_return", (df.close - df.open) / df.open)
 
-label_stringIdx = StringIndexer(inputCol='target', outputCol='label')
-stages += [label_stringIdx]
+# 3. Calculate intra-day volatility - difference between the highest and lowest prices of the day
+df = df.withColumn("intra_day_volatility", df.high - df.low)
 
-assembler = VectorAssembler(inputCols=[c + "classVec" for c in categoricalColumns], outputCol="features")
-stages += [assembler]
+# 4. Calculate daily volatility - change in closing price from the previous day
+df = df.withColumn("daily_volatility", df.close - df.lag_close_1)
 
-pipeline = Pipeline(stages=stages)
-pipelineModel = pipeline.fit(df)
-df = pipelineModel.transform(df)
+# 5. Calculate a 7-day moving average for the closing prices
+df = df.withColumn("7_day_avg_close", F.avg(df.close).over(w.rowsBetween(-6, 0)))
 
-df.select('close', 'label', 'features').show()
-(trainingData, testData) = df.randomSplit([0.8, 0.2])
+# 6. Binary indicator if there was a dividend
+df = df.withColumn("is_dividend", F.when(df["dividend amount"] > 0, 1).otherwise(0))
 
-dr = DecisionTreeRegressor(labelCol="label", featuresCol="features")
+# 7. Binary indicator if there was a stock split
+df = df.withColumn("is_split", F.when(df["split coefficient"] != 1, 1).otherwise(0))
 
-model = dt.fit(trainingData)
-predictions = model.transform(testData)
+# Drop any rows with NA values (which might have been introduced due to lagging operations)
+df = df.dropna()
 
-evaluator = RegressionEvaluator(labelCol="label", predictionCol="prediction", metricName="rmse")
-rmse = evaluator.evaluate(predictions)
-print("Root Mean Squared Error (RMSE) on test data = %g" % rmse)
+# Assemble the features into a single vector for MLlib consumption
+feature_columns = ["open", "high", "low", "close", "volume", "daily_return", 
+                   "intra_day_volatility", "daily_volatility", "7_day_avg_close", 
+                   "is_dividend", "is_split"]
+assembler = VectorAssembler(inputCols=feature_columns, outputCol="features")
+df_assembled = assembler.transform(df)
+
+# Split the data into a training set and a test set. 
+# Here, we're using a time-based split - 80% for training and 20% for testing
+train_count = int(df_assembled.count() * 0.8)
+train_data = df_assembled.orderBy("date").limit(train_count)
+test_data = df_assembled.subtract(train_data)
+
+# Model Training
+# Gradient Boosted Trees are chosen due to their robustness and ability to model non-linear patterns
+gbt = GBTRegressor(labelCol="adjusted close", featuresCol="features", maxIter=50)
+model = gbt.fit(train_data)
+predictions = model.transform(test_data)
+
+# Model Evaluation
+# Compute the RMSE (Root Mean Squared Error) for the predictions
+evaluator_rmse = RegressionEvaluator(labelCol="adjusted close", predictionCol="prediction", metricName="rmse")
+rmse = evaluator_rmse.evaluate(predictions)
+print("Root Mean Squared Error (RMSE) on test data =", rmse)
+
+# Compute other metrics: Mean Absolute Error (MAE) and R-squared (R2)
+for metric in ["mae", "r2"]:
+    evaluator = RegressionEvaluator(labelCol="adjusted close", predictionCol="prediction", metricName=metric)
+    value = evaluator.evaluate(predictions)
+    print(f"{metric.upper()}: {value}")
+
+# Visualization
+# Plot the actual vs predicted values for adjusted closing prices
+preds = predictions.select("adjusted close", "prediction").toPandas()
+plt.figure(figsize=(12, 6))
+plt.plot(preds["adjusted close"], label='Actual', color='blue')
+plt.plot(preds["prediction"], label='Predicted', color='red', alpha=0.6)
+plt.title('Actual vs Predicted Adjusted Closing Prices')
+plt.xlabel('Days')
+plt.ylabel('Adjusted Closing Price')
+plt.legend()
+plt.show()
+
+# Terminate the Spark session
+spark.stop()
